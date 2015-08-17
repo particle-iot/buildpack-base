931298e0653bb7e7d950dfe1f67b06faca208735
Wojtek Siudzinski <admin@suda.pl>
2015-08-17 17:25:27 +0100

    Fix command order

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,12 +4,12 @@ TIME_START=`date +%s.%N`
 # Load helper functions
 for file in lib/*.bash ; do source "$file"; done
 
-# Copy input files to workspace
-cp -r $INPUT_DIR $WORKSPACE_DIR
-
 # Setup constants
 run-hook /hooks/env
 run-hook /hooks/post-env
+
+# Copy input files to workspace
+cp -r $INPUT_DIR/. $WORKSPACE_DIR
 
 # Start logging to file
 exec > >(tee $OUTPUT_DIR/run.log)
