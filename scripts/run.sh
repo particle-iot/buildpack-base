658f1f79f03e1327573c64ee98840fe551836243
Wojtek Siudzinski <admin@suda.pl>
2015-08-17 17:19:24 +0100

    Copy input files to workspace

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,9 @@ TIME_START=`date +%s.%N`
 # Load helper functions
 for file in lib/*.bash ; do source "$file"; done
 
+# Copy input files to workspace
+cp -r $INPUT_DIR $WORKSPACE_DIR
+
 # Setup constants
 run-hook /hooks/env
 run-hook /hooks/post-env
